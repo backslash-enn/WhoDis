f504b55af3d2f91a10dff5866be1871e7cf2c11f
Catalina <catalinamorales@knights.ucf.edu>
2019-09-19 22:32:06 -0400

    more frontend improvements
    -added repeated animation
    -added test profile pic
    -renamed html classes for clarity
    -changed white to smokewhite
    -cleaned page layout in css

--- new frontend/WhoDis/functionality.js
+++ new frontend/WhoDis/functionality.js
@@ -1,35 +1,43 @@
 function displayContactInfo(b){
 
-    var contact_info = document.getElementById('contactinfo');
+    var contact_details = document.getElementById('contactdetails');
     var welcome_msg = document.getElementById('welcome');
+    var left_panel = document.getElementById('leftpanel');
     switch (b.id){
         case "Harry Otter":
-            if (contact_info.style.display === "block")
+            if (contact_details.style.display === "block")
             {
-                contact_info.style.display = "none";
-                welcome_msg.style.display = "block";
+                left_panel.style.animation = 'none';
+                left_panel.offsetHeight;
+
+                left_panel.style.animation = "swap-leftpanel-slide .4s linear forwards";
+                left_panel.style.animationTimingFunction = "cubic-bezier(0, .85, .31, .99)";
+
+                setTimeout(function() {
+                    contact_details.style.display = "none";
+                    welcome_msg.style.display = "block";
+                }, 100);
             }
             else
             {
-                contact_info.style.display = "block";
-                welcome_msg.style.display = "none";
+                left_panel.style.animation = 'none';
+                left_panel.offsetHeight;
+                
+                left_panel.style.animation = "swap-leftpanel-slide .4s linear forwards";
+                left_panel.style.animationTimingFunction = "cubic-bezier(0, .85, .31, .99)";
+
+                setTimeout(function() {
+                    contact_details.style.display = "block";
+                    welcome_msg.style.display = "none";
+                }, 100);
             } 
             break;
         case "Harry Otter 2":
-            if (contact_info.style.display === "block")
+            if (contact_details.style.display === "block")
                 contact_info.style.display = "none";
             else   
-                contact_info.style.display = "block";
+                contact_details.style.display = "block";
             break;
     }
-    // if (x.style.display === "block"){
-    //     x.style.opacity = '0';
-    //     x.style.display = "none";
-    // }
-    // else {
-    //     x.style.display = "block";
-    //     x.style.opacity = '1';
-    // }
-
 }
 
